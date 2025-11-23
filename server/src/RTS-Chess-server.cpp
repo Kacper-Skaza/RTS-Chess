a86e26c2d8f9dd483e128638e839b34f42d2c980
Kacper Skaza <kacper.skaza.100@gmail.com>
2025-11-23 19:13:30 +0100

    Add placeholder to main files

---
+++ server/src/RTS-Chess-server.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "../../shared/headers/Board.hpp"
+#include "../../shared/headers/pieces/Pawn.hpp"
+
+int main()
+{
+	Board board = {};
+	std::cout << ((Pawn *)(board.getBoardFull()[1][1].get()))->enPassant ? 1 : 0;
+	return 0;
+}

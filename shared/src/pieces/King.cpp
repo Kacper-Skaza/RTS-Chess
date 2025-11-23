7ce1ebfd8391f88f41bee3539b652234a4b09252
Kacper Skaza <kacper.skaza.100@gmail.com>
2025-11-23 17:08:25 +0100

    Add castling

--- shared/src/pieces/King.cpp
+++ shared/src/pieces/King.cpp
@@ -33,6 +33,15 @@ bool King::validateMove(const Move &move, const std::vector<std::vector<std::uni
 	if (piece != this)
 		return false;
 
+	// Check if move is castling
+	if (std::toupper(target->getSymbol()) == 'R' &&
+		std::isupper(piece->getSymbol()) == std::isupper(target->getSymbol()) &&
+		piece->getFirstMove() == true &&
+		target->getFirstMove() == true)
+	{
+		return true;
+	}
+
 	// Check if move is one square in any direction
 	if (!(dx <= 1 && dy <= 1 && (dx != 0 || dy != 0)))
 		return false;

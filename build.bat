7966f43e7bf4b554009d09ac8cfbb9ef989cfc66
destrojer <101801467+destrojer@users.noreply.github.com>
2025-11-30 12:52:02 +0100

    update build.bat

--- build.bat
+++ build.bat
@@ -3,6 +3,8 @@ if exist build rmdir /s /q build
 mkdir build
 
 @echo on
-cmake -S . -B build
-cmake --build build --config Release
+@REM cmake -S . -B build -G "MinGW Makefiles" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Debug
+cmake -S . -B build -G "Ninja" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Debug
+
+cmake --build build
 cmake --install build

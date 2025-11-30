32cca05e2b4f87a3ac3b5b74aef9a5a63c41cc27
destrojer <101801467+destrojer@users.noreply.github.com>
2025-11-30 14:17:33 +0100

    changed to unordere_map from vector

--- shared/headers/Room.hpp
+++ shared/headers/Room.hpp
@@ -5,11 +5,13 @@
 #include "Structures.hpp"
 
 #include <cstdlib>
+#include <cstdint>
 #include <utility>
 #include <cctype>
 #include <vector>
 #include <stdexcept>
 #include <memory>
+#include <unordered_map>
 
 class Room
 {
@@ -17,7 +19,7 @@ private:
     bool matchStarted = false;
     uint8_t maxPlayerCount;
     Board board;
-    std::vector<User> userList;
+    std::unordered_map<unsigned int, User> userList;
     std::vector<unsigned int> playerList;
 
 public:
@@ -27,14 +29,14 @@ public:
 
     // Get structure Data
     bool isMatchReady() const;
-    const bool isMatchStarted() const noexcept;
-    const Board &getBoard() const noexcept;
-    const unsigned int getUserCount() const;
-    const unsigned int getPlayerCount() const;
-    const unsigned int getPlayerReadyCount() const;
-    const uint8_t getMaxPlayerCount() const noexcept;
-    const std::vector<User> &getUserList() const;
-    const std::vector<User> &getPlayerList() const;
+    bool isMatchStarted() const noexcept;
+    Board &getBoard();
+    unsigned int getUserCount() const;
+    unsigned int getPlayerCount() const;
+    unsigned int getPlayerReadyCount() const;
+    uint8_t getMaxPlayerCount() const noexcept;
+    std::unordered_map<unsigned int, User> &getUserList();
+    std::unordered_map<unsigned int, User> &getPlayerList();
 
     // Set structure data
     void addUserToRoom(User& joining);

7f6038c9a7b9870f1e690a25e1e9189c5fcbc2e0
destrojer <101801467+destrojer@users.noreply.github.com>
2025-12-06 21:27:51 +0100

    add example code for textbox class

--- client/src/RTS-Chess-client.cpp
+++ client/src/RTS-Chess-client.cpp
@@ -6,6 +6,8 @@
 #include <SDL2/SDL_image.h>
 
 #include "../headers/SDLTextureManager.hpp"
+#include "../headers/SDLFontManager.hpp"
+#include "../headers/TextBox.hpp"
 
 int main()
 {
@@ -25,7 +27,6 @@ int main()
 	{
 		std::cerr << "TTF_Init Error: " << TTF_GetError() << "\n";
 	}
-	
 
 	SDL_Window *window = SDL_CreateWindow("Puste Okno", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
 	if (!window)
@@ -61,37 +62,71 @@ int main()
 
 	SDL_Rect dst{100, 100, w, h};
 
-	// --- Main loop ---
-	bool running = true;
-	SDL_Event event;
-
-	while (running)
+	//Test for textbox
+	//Scope to satisfy destructor
 	{
-		while (SDL_PollEvent(&event))
+		SDLFontManager fontMan(renderer);
+		SDL_Rect rect{100, 100, 200, 50};
+		int x, y;
+		TextBox box(window, renderer, &fontMan, rect, "Roboto/Roboto-Medium", 20);
+
+		// --- Main loop ---
+		bool running = true;
+		SDL_Event event;
+
+		while (running)
 		{
-			// Close window event
-			if (event.type == SDL_QUIT)
+			while (SDL_PollEvent(&event) != 0)
 			{
-				running = false;
+				// Close window event
+				if (event.type == SDL_QUIT)
+				{
+					running = false;
+				}
+				
+				if(event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT)
+				{
+					SDL_GetMouseState(&x, &y);
+					if (box.checkIfClicked(x, y))
+					{
+						SDL_StartTextInput();
+					}
+					else
+					{
+						SDL_StopTextInput();
+					}
+				}
+				if (SDL_IsTextInputActive() == SDL_TRUE)
+				{
+					box.textListener(event);
+				}
 			}
+
+			// Clear screen
+			SDL_RenderClear(renderer);
+
+			// Draw texture
+			SDL_RenderCopy(renderer, coconut, nullptr, &dst);
+			
+			//Test for textBox
+			SDL_SetRenderDrawColor(renderer, 150, 150, 150, 255);
+			SDL_RenderFillRect(renderer, &rect);
+			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+			box.genTexture();
+			SDL_RenderCopy(renderer, box.getTexture(), nullptr, &(box.getTextureRect()));
+
+			// Present to screen
+			SDL_RenderPresent(renderer);
+
+			// 60 FPS <- should implement delta time for more stable fps
+			SDL_Delay(16);
 		}
-
-		// Clear screen
-		SDL_RenderClear(renderer);
-
-		// Draw texture
-		SDL_RenderCopy(renderer, coconut, nullptr, &dst);
-
-		// Present to screen
-		SDL_RenderPresent(renderer);
-
-		// 60 FPS
-		SDL_Delay(16);
 	}
 
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	IMG_Quit();
+	TTF_Quit();
 	SDL_Quit();
 	return 0;
 }

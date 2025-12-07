66bfc0f3ad8303e50aabb97b094507ec513a063e
destrojer <101801467+destrojer@users.noreply.github.com>
2025-12-07 01:31:00 +0100

    add regex and initial methods

--- client/headers/views/ConnectView.hpp
+++ client/headers/views/ConnectView.hpp
@@ -2,14 +2,34 @@
 
 #include <vector>
 #include <memory>
+#include <regex>
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 
 #include "../SDLFontManager.hpp"
 #include "../SDLTextureManager.hpp"
+#include "../TextBox.hpp"
 
 class ConnectView
 {
+private:
+    SDL_Window* window;
+    SDL_Renderer* renderer;
     
+    TextBox connectionBox;
+    SDL_Rect connectButton;
+
+    static const std::regex connectionRule;
+
+    void drawButton();
+public:
+    explicit ConnectView(SDL_Window* window, SDL_Renderer* renderer, SDLFontManager* fontManager);
+    ~ConnectView() = default;
+
+    void render();
+    void inputSanatizer();
+
+    bool validateConnectionData();
+    bool connect();
 };
--- client/src/views/ConnectView.cpp
+++ client/src/views/ConnectView.cpp
@@ -1,2 +1,34 @@
 #include "../../headers/views/ConnectView.hpp"
 
+const std::regex ConnectView::connectionRule("^((1[0-9]{2}|[0-9]{1,2}|2[0-4][0-9]|25[0-5])\\.){3}(1[0-9]{2}|[0-9]{1,2}|2[0-4][0-9]|25[0-5]):([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$", std::regex::optimize);
+
+ConnectView::ConnectView(SDL_Window *window, SDL_Renderer *renderer, SDLFontManager* fontManager)
+    : connectionBox(window, renderer, fontManager, {100, 100, 300, 50}, "Roboto/Roboto-Medium", 20)
+{
+    this->window = window;
+    this->renderer = renderer;
+}
+
+void ConnectView::render()
+{
+    
+}
+
+void ConnectView::inputSanatizer()
+{
+
+}
+
+/**
+ * This method may be unoptimized
+ */
+bool ConnectView::validateConnectionData()
+{
+    return std::regex_match(this->connectionBox.getString(), this->connectionRule);
+}
+
+bool ConnectView::connect()
+{
+    //pure intenet function
+    return false;
+}

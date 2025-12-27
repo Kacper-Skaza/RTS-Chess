da32eb42411669eef0acf39fc77406ab4bd25e00
destrojer <101801467+destrojer@users.noreply.github.com>
2025-12-27 12:06:36 +0100

    change contructor change firstmove to movecount

--- shared/headers/pieces/Piece.hpp
+++ shared/headers/pieces/Piece.hpp
@@ -11,19 +11,22 @@ class Move;
 
 class Piece
 {
-private:
+protected:
 	const char SYMBOL;
-	bool firstMove = true;
+	int moveCount = 0;
+	const int ID; 
 
 	// TO BE DETERMINED LATER (INT COZ DEPENDANT ON MS)
 	int cooldown = 2000;
 
 public:
 	Piece();
+	Piece(const char SIDE, const char SYMBOL, const int ID);
 	virtual ~Piece() = default;
 
+	int  getID() const noexcept;
 	char getSymbol() const noexcept;
-	bool getFirstMove() const noexcept;
+	bool getMoveCount() const noexcept;
 
 	void makeMove();
 	virtual bool validateMove(const Move &move, const std::vector<std::vector<Piece *>> &board) const;
--- shared/src/pieces/Piece.cpp
+++ shared/src/pieces/Piece.cpp
@@ -1,20 +1,28 @@
 #include "../../headers/pieces/Piece.hpp"
+#include "Piece.hpp"
 
-Piece::Piece() : SYMBOL('P') {}
+Piece::Piece() : SYMBOL('P'), ID(00) {}
+
+Piece::Piece(const char SIDE, const char SYMBOL, const int ID): SYMBOL(std::toupper(SIDE) == 'W'? std::toupper(SYMBOL): std::tolower(SYMBOL)), ID(ID) {}
 
 char Piece::getSymbol() const noexcept
 {
 	return SYMBOL;
 }
 
-bool Piece::getFirstMove() const noexcept
+int Piece::getID() const noexcept
 {
-	return firstMove;
+    return ID;
+}
+
+bool Piece::getMoveCount() const noexcept
+{
+	return moveCount;
 }
 
 void Piece::makeMove()
 {
-	firstMove = false;
+	moveCount++;
 }
 
 bool Piece::validateMove(const Move &move, const std::vector<std::vector<Piece *>> &board) const

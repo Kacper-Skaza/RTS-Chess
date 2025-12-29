787507b30b57d7d0ea0ef9c88a3d1e58aa0f65f6
destrojer <101801467+destrojer@users.noreply.github.com>
2025-12-29 13:50:37 +0100

    new testable version

--- client/src/RTS-Chess-client.cpp
+++ client/src/RTS-Chess-client.cpp
@@ -43,7 +43,8 @@ int main()
 		std::cerr << "TTF_Init Error: " << TTF_GetError() << "\n";
 	}
 
-	SDL_Window *window = SDL_CreateWindow("Puste Okno", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+	SDL_Window *window = SDL_CreateWindow("RTS Chess", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1920, 1080,
+		 SDL_WINDOW_SHOWN);
 	if (!window)
 	{
 		std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
@@ -59,6 +60,8 @@ int main()
 		SDL_Quit();
 		return 1;
 	}
+
+	SDL_RenderSetLogicalSize(renderer, 1920, 1080);
 
 	// --- Texture Manager ---
 	SDLTextureManager texMgr(renderer);
@@ -81,7 +84,7 @@ int main()
 		SDLFontManager fontMan(renderer);
 		int x, y;
 
-		std::unique_ptr<View> currentView = std::make_unique<ConnectView>(window, renderer, &fontMan);
+		std::unique_ptr<View> currentView = std::make_unique<GameView>(window, renderer, &fontMan, &texMgr);
 
 		// --- Main loop ---
 		bool running = true;
@@ -89,56 +92,77 @@ int main()
 
 		while (running)
 		{
-			while (SDL_PollEvent(&event) != 0)
-			{
-				// Close window event
-				if (event.type == SDL_QUIT)
-				{
-					running = false;
-				}
+			mainLoop(window, renderer, &fontMan, currentView, event);
+			// while (SDL_PollEvent(&event) != 0)
+			// {
+			// 	// Close window event
+			// 	if (event.type == SDL_QUIT)
+			// 	{
+			// 		running = false;
+			// 	}
+
+			// 	if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_F12)
+			// 	{
+			// 		if (SDL_GetWindowFlags(window) & SDL_WINDOW_FULLSCREEN_DESKTOP)
+			// 		{
+			// 			SDL_SetWindowFullscreen(window, 0);
+			// 			SDL_SetWindowSize(window, 1920, 1080);
+			// 			SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+			// 		}
+			// 		else
+			// 		{
+			// 			SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP);
+			// 		}
+			// 	}
 				
-				if(event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT)
-				{
-					SDL_GetMouseState(&x, &y);
-					//check if connectView
-					if (ConnectView* conView = dynamic_cast<ConnectView*>(currentView.get()))
-					{
-						if (conView->getConnectionBox().checkIfClicked(x, y))
-						{
-							SDL_StartTextInput();
-						}
-						else
-						{
-							SDL_StopTextInput();
-						}
-						if (conView->getConnectButton().checkIfClicked(x,y))
-						{
-							if (conView->validateConnectionData())
-							{
-								currentView.release();
-								currentView = std::make_unique<LobbyView>();
-							}
-							else
-							{
-								conView->getConnectionBox().setText("");
-							}							
-						}
+				
+			// 	if(event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT)
+			// 	{
+			// 		SDL_GetMouseState(&x, &y);
+			// 		//check if connectView
+			// 		if (ConnectView* conView = dynamic_cast<ConnectView*>(currentView.get()))
+			// 		{
+			// 			if (conView->getConnectionBox().checkIfClicked(x, y))
+			// 			{
+			// 				SDL_StartTextInput();
+			// 			}
+			// 			else
+			// 			{
+			// 				SDL_StopTextInput();
+			// 			}
+			// 			if (conView->getConnectButton().checkIfClicked(x,y))
+			// 			{
+			// 				if (conView->validateConnectionData())
+			// 				{
+			// 					currentView.release();
+			// 					currentView = std::make_unique<LobbyView>();
+			// 				}
+			// 				else
+			// 				{
+			// 					conView->getConnectionBox().setText("");
+			// 				}							
+			// 			}
 						
-					}
-				}
-				if (SDL_IsTextInputActive() == SDL_TRUE)
-				{
-					if (ConnectView* conView = dynamic_cast<ConnectView*>(currentView.get()))
-					{
-						conView->getConnectionBox().textListener(event);
-					}
-				}
-			}
+			// 		}
+			// 	}
+			// 	if (SDL_IsTextInputActive() == SDL_TRUE)
+			// 	{
+			// 		if (ConnectView* conView = dynamic_cast<ConnectView*>(currentView.get()))
+			// 		{
+			// 			conView->getConnectionBox().textListener(event);
+			// 		}
+			// 	}
+			// 	if (GameView* gameView = dynamic_cast<GameView*>(currentView.get()))
+			// 	{
+			// 		gameView->handleEvent(event);
+			// 	}
+				
+			// }
 
-			currentView.get()->render();			
+			// currentView.get()->render();			
 
-			// 60 FPS <- should implement delta time for more stable fps
-			SDL_Delay(16);
+			// // 60 FPS <- should implement delta time for more stable fps
+			// SDL_Delay(16);
 		}
 	}
 

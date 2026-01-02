2ebc9ebc6af43af1df605386b9da0b01132037e9
destrojer <101801467+destrojer@users.noreply.github.com>
2026-01-02 02:47:08 +0100

    add functionality to lobby view add update functionality every 10 frames

--- client/src/RTS-Chess-MainLoop.cpp
+++ client/src/RTS-Chess-MainLoop.cpp
@@ -9,6 +9,8 @@ namespace
     bool boxIp = true;
     int mousePosX;
     int mousePosY;
+    int toUpdateCounter = 10;
+    std::string messageContainer = "";
 }
 
 template<typename T> bool between(T x, T a, T b)
@@ -55,15 +57,22 @@ void connectLoop(SDL_Window* window, SDL_Renderer* renderer, SDLFontManager* fon
                                 {"type", "REQUEST_NICK"},
                                 {"data", {"nick", connectView->getUserBox().getString()}}
                             };
-                            MessageHandler::handleView(connectView, connectionManager, me, j);
+                            MessageHandler::handleView(connectView, connectionManager, me, j.dump());
                             unsigned long long id = 0;
                             me = new User(id, connectView->getUserBox().getString());
                         }
+                        nlohmann::json j = nlohmann::json{
+                            {"type", "REQUEST_ROOMS"},
+                            {"data", nullptr}
+                        };
+                        MessageHandler::handleView(connectView, connectionManager, me, j.dump());
                         view.release();
                         view = std::make_unique<LobbyView>(window, renderer, fontManager);
                     }
                     else
+                    {
                         connectView->getConnectionBox().setText("");
+                    }
                 }
                 else
                 {
@@ -77,7 +86,91 @@ void connectLoop(SDL_Window* window, SDL_Renderer* renderer, SDLFontManager* fon
                 connectView->getConnectionBox().textListener(event);
             else
                 connectView->getUserBox().textListener(event);
+        }
+    }
+}
 
+void lobbyLoop(SDL_Window* window, SDL_Renderer* renderer, SDLFontManager* fontManager, std::unique_ptr<View> &view, LobbyView* lobbyView, SDL_Event& event)
+{
+    
+    // User dummyCreator(1ULL, "SystemAdmin");
+
+    // // 2. Create a vector of Rooms
+    // std::vector<Room> testRooms;
+
+    // // 3. Add rooms to the vector
+    // // We use emplace_back to construct the Room directly in the vector
+    // testRooms.emplace_back(Room("Pro Only 2000+", dummyCreator));
+    // testRooms.emplace_back(Room("Fast Blitz 3min", dummyCreator));
+    // testRooms.emplace_back(Room("Casual Game", dummyCreator));
+    // testRooms.emplace_back(Room("Casual Game", dummyCreator));
+    // testRooms.emplace_back(Room("Casual Game", dummyCreator));
+    // testRooms.emplace_back(Room("Casual Game", dummyCreator));
+    // testRooms.emplace_back(Room("Casual Game", dummyCreator));
+    // testRooms.emplace_back(Room("Casual Game", dummyCreator));
+    // testRooms.emplace_back(Room("Casual Game", dummyCreator));
+    // testRooms.emplace_back(Room("Casual Game", dummyCreator));
+    // testRooms.emplace_back(Room("Casual Game", dummyCreator));
+    // testRooms.emplace_back(Room("Casual Game", dummyCreator));
+    // testRooms.emplace_back(Room("Casual Game", dummyCreator));
+    // testRooms.emplace_back(Room("Casual Game", dummyCreator));
+    // testRooms.emplace_back(Room("Casual Game", dummyCreator));
+    // testRooms.emplace_back(Room("Casual Game", dummyCreator));
+    // testRooms.emplace_back(Room("Casual Game", dummyCreator));
+    // testRooms.emplace_back(Room("Casual Game", dummyCreator));
+    // testRooms.emplace_back(Room("Casual Game", dummyCreator));
+    // testRooms.emplace_back(Room("Casual Game", dummyCreator));
+    // testRooms.emplace_back(Room("Casual Game", dummyCreator));
+    // testRooms.emplace_back(Room("Casual Game", dummyCreator));
+    // testRooms.emplace_back(Room("Casual Game", dummyCreator));
+    // testRooms.emplace_back(Room("Casual Game1", dummyCreator));
+    // lobbyView->updateRooms(testRooms);
+
+    while (SDL_PollEvent(&event) != 0)
+    {
+        if (event.type == SDL_QUIT)
+        {
+            running = false;
+        }
+        if (event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT)
+        {
+            SDL_GetMouseState(&mousePosX, &mousePosY);
+            Room* room = lobbyView->getRoomClicked();
+            if (room != nullptr)
+            {
+                //here join room
+                nlohmann::json j = nlohmann::json{
+                    {"type", "ROOM_JOIN"},
+                    {"data", {"room_name", room->getRoomName()}}
+                };
+                MessageHandler::handleView(lobbyView, connectionManager, me, j.dump());
+                view.release();
+                view = std::make_unique<RoomView>(window, renderer, fontManager);
+            }
+            if (lobbyView->getCreateButton().checkIfClicked(mousePosX, mousePosY)) 
+            {
+                //here create room check
+                nlohmann::json j = nlohmann::json{
+                    {"type", "ROOM_CREATE"},
+                    {"data", {"room_name", lobbyView->getcreateBox().getString()}}
+                };
+                MessageHandler::handleView(lobbyView, connectionManager, me, j.dump());
+                view.release();
+                view = std::make_unique<RoomView>(window, renderer, fontManager);
+            }
+            if (lobbyView->getcreateBox().checkIfClicked(mousePosX, mousePosY))
+                SDL_StartTextInput();
+            else
+                SDL_StopTextInput();
+        }
+        if (event.type == SDL_MOUSEWHEEL)
+        {
+            // event.wheel.y to kierunek (1 w górę, -1 w dół)
+            lobbyView->handleScroll(event.wheel.y);
+        }
+        if (SDL_IsTextInputActive() == SDL_TRUE)
+        {
+            lobbyView->getcreateBox().textListener(event);
         }
     }
 }
@@ -115,64 +208,6 @@ void roomLoop(SDL_Window* window, SDL_Renderer* renderer, SDLFontManager* fontMa
 
 }
 
-void lobbyLoop(SDL_Window* window, SDL_Renderer* renderer, SDLFontManager* fontManager, std::unique_ptr<View> &view, LobbyView* lobbyView, SDL_Event& event)
-{
-    /*
-    User dummyCreator(1ULL, "SystemAdmin");
-
-    // 2. Create a vector of Rooms
-    std::vector<Room*> testRooms;
-
-    // 3. Add rooms to the vector
-    // We use emplace_back to construct the Room directly in the vector
-    testRooms.emplace_back(new Room("Pro Only 2000+", dummyCreator));
-    testRooms.emplace_back(new Room("Fast Blitz 3min", dummyCreator));
-    testRooms.emplace_back(new Room("Casual Game", dummyCreator));
-    testRooms.emplace_back(new Room("Casual Game", dummyCreator));
-    testRooms.emplace_back(new Room("Casual Game", dummyCreator));
-    testRooms.emplace_back(new Room("Casual Game", dummyCreator));
-    testRooms.emplace_back(new Room("Casual Game", dummyCreator));
-    testRooms.emplace_back(new Room("Casual Game", dummyCreator));
-    testRooms.emplace_back(new Room("Casual Game", dummyCreator));
-    testRooms.emplace_back(new Room("Casual Game", dummyCreator));
-    testRooms.emplace_back(new Room("Casual Game", dummyCreator));
-    testRooms.emplace_back(new Room("Casual Game", dummyCreator));
-    testRooms.emplace_back(new Room("Casual Game", dummyCreator));
-    testRooms.emplace_back(new Room("Casual Game", dummyCreator));
-    testRooms.emplace_back(new Room("Casual Game", dummyCreator));
-    testRooms.emplace_back(new Room("Casual Game", dummyCreator));
-    testRooms.emplace_back(new Room("Casual Game", dummyCreator));
-    testRooms.emplace_back(new Room("Casual Game", dummyCreator));
-    testRooms.emplace_back(new Room("Casual Game", dummyCreator));
-    testRooms.emplace_back(new Room("Casual Game", dummyCreator));
-    testRooms.emplace_back(new Room("Casual Game", dummyCreator));
-    testRooms.emplace_back(new Room("Casual Game", dummyCreator));
-    testRooms.emplace_back(new Room("Casual Game", dummyCreator));
-    testRooms.emplace_back(new Room("Casual Game", dummyCreator));
-    lobbyView->updateRooms(testRooms);*/
-
-    while (SDL_PollEvent(&event) != 0)
-    {
-        if (event.type == SDL_QUIT)
-        {
-            running = false;
-        }
-        else if (event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT)
-        {
-            Room* room = lobbyView->getRoomClicked();
-            if (room != nullptr)
-            {
-                std::cout<<room->getRoomName()<<std::endl;
-            }
-        }
-        else if (event.type == SDL_MOUSEWHEEL)
-        {
-            // event.wheel.y to kierunek (1 w górę, -1 w dół)
-            lobbyView->handleScroll(event.wheel.y);
-        }
-    }
-}
-
 void gameLoop(SDL_Window* window, SDL_Renderer* renderer, SDLFontManager* fontManager, std::unique_ptr<View> &view, GameView* gameView, SDL_Event& event)
 {
     while (SDL_PollEvent(&event) != 0)
@@ -207,23 +242,27 @@ void gameLoop(SDL_Window* window, SDL_Renderer* renderer, SDLFontManager* fontMa
                 }
             }
         }
-        if (event.type == SDL_MOUSEBUTTONDOWN && SDL_MOUSEMOTION)
-        {
-            //maybe implemented later
-        }
-
     }
 }
 
 void delay()
 {
     //add delta time
-
-
     SDL_Delay(1000/TARGET_FPS);
 }
 
-void mainLoop(SDL_Window* window, SDL_Renderer* renderer, SDLFontManager* fontManager, std::unique_ptr<View> &view, SDL_Event& event)
+void receiveLoop(std::unique_ptr<View> &view)
+{
+    messageContainer = connectionManager->recvMessage();
+    while (messageContainer.empty() == false)
+    {
+        MessageHandler::handleView(view.get(), connectionManager, me, messageContainer);
+        messageContainer = connectionManager->recvMessage();
+    }
+    messageContainer = "";
+}
+
+void mainLoop(SDL_Window* window, SDL_Renderer* renderer, SDLFontManager* fontManager, SDLTextureManager* textureManager, std::unique_ptr<View> &view, SDL_Event& event)
 {
     if (GameView* gameView = dynamic_cast<GameView*>(view.get()))
         gameLoop(window, renderer, fontManager, view, gameView, event);
@@ -236,13 +275,23 @@ void mainLoop(SDL_Window* window, SDL_Renderer* renderer, SDLFontManager* fontMa
     else
         throw std::logic_error("View does not exist");
     view.get()->render();
+    if (connectionManager != nullptr)
+    {
+        receiveLoop(view);   
+        if (toUpdateCounter-- <= 0)
+        {
+            connectionManager->update();
+            toUpdateCounter = 10;
+        }
+    }
     delay();
+    if (connectionManager != nullptr)
+        receiveLoop(view);
     return;
 }
 
 void mainLoopDestroy()
 {
-    
     if (me != nullptr)
         delete me;
     if (connectionManager != nullptr)
